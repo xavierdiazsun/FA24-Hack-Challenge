eb95b0aff0babdc67e61d17fc118d266f33aaa6a
Dicky <dn337@cornell.edu>
2024-12-04 17:42:26 -0500

    imported dependencies

---
+++ frontend/takeout/takeout/Networking/NetworkManager.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.swift
+//  takeout
+//
+//  Created by Dicky Ning on 12/4/24.
+//
+
+import Alamofire
+
+class NetworkManager {
+    static let shared = NetworkManager()
+
+    private init() {}
+
+    
+}
+
